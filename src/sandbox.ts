f7c8656aa1a1dcc39db19baa9544527aa0c7bef0
Sidd Singal <ssingal05@gmail.com>
2023-01-26 00:52:42 -0800

    update

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,16 +1,99 @@
-import { Client } from "./client"
+import { CadenceParser } from "@onflow/cadence-parser"
+import * as fcl from "@onflow/fcl"
+import { promises as fs } from "fs"
 
-const ACCOUNT = "7ec1f607f0872a9e"
+const CODE_ONE = `
+import Blah from "./Blah.cdc"
 
-const main = async () => {
-  const client = Client.mainnet()
-  const account = await client.account({ address: ACCOUNT })
-  console.log(account)
-  console.log(account.body.keys)
-
-  const contracts = await client.contracts({ address: ACCOUNT })
-  console.log(contracts.body.balance)
-  console.log(Object.keys(contracts.body.contracts))
+pub struct Thing {
+  priv let a: UInt64
+  priv let b: UInt64
+  init() {
+    self.a = 1
+    self.b = 2
+  }
 }
 
-main()
+// Func
+pub fun main(thing: Thing): Blah.Foo {
+  return thing
+}
+`
+
+const CODE_TWO = `
+pub struct THING {}
+pub fun main(): FOO.THING.YO.BLAH {}
+`
+
+const CODE_THREE = `
+import FungibleToken from 0xee82856bf20e2aa6
+
+pub fun main(): AnyStruct {
+  return Type<FungibleToken.Vault>()
+}
+`
+
+const main = async () => {
+  fcl.config().put("accessNode.api", "http://127.0.0.1:8888")
+
+  const response = await fcl.query({
+    cadence: `
+      pub struct Thing {
+        priv let a: UInt64
+        priv let b: UInt64
+        init() {
+          self.a = 1
+          self.b = 2
+        }
+      }
+
+      pub fun main(thing: Thing): AnyStruct {
+        return thing
+      }
+    `,
+    args: (arg: any, t: any) => [
+      arg(
+        {
+          fields: [
+            { name: "a", value: "5" },
+            { name: "b", value: "6" },
+          ],
+        },
+        t.Struct(
+          "s.0450233b4f5b6c3c515d9288594891f6cea9c36edf3520e050aa1a455bf3bf77.Thing",
+          [
+            { name: "a", value: t.UInt64 },
+            { name: "b", value: t.UInt64 },
+          ],
+        ),
+      ),
+    ],
+  })
+
+  console.log(response)
+}
+
+const main3 = async () => {
+  const parserBinary = await fs.readFile("cadence-parser.wasm")
+  const parser = await CadenceParser.create(parserBinary)
+
+  const ast = parser.parse(CODE_TWO)
+
+  console.log(JSON.stringify(ast, null, 2))
+}
+
+main3()
+
+// import { Client } from "./client"
+// const ACCOUNT = "7ec1f607f0872a9e"
+
+// const main = async () => {
+//   const client = Client.mainnet()
+//   const account = await client.account({ address: ACCOUNT })
+//   console.log(account)
+//   console.log(account.body.keys)
+
+//   const contracts = await client.contracts({ address: ACCOUNT })
+//   console.log(contracts.body.balance)
+//   console.log(Object.keys(contracts.body.contracts))
+// }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,98 @@
 import { z } from "zod"
 
-const Void_ = z
-  .object({
-    type: z.literal("Void"),
-  })
-  .transform(() => undefined).parse
+class CannotBeEncoded extends Error {}
+class CannotBeDecoded extends Error {}
+class MissingTag extends Error {}
 
-const t: any = {
-  type: "Void",
+type CompositeTag = {
+  contract?: string
+  name: string
 }
 
-const parsed = Void_(t)
+type IdentifierResolver = (tag: CompositeTag) => string
 
-console.log(parsed)
+type Codec<A, Z> = {
+  encode: (a: A, resolve?: IdentifierResolver) => any
+  decode: (a: any) => A
+  zodDecoder: Z
+}
+
+const buildZodCodec =
+  <A>(encode: (a: A, resolve?: IdentifierResolver) => any) =>
+  <Z extends z.ZodObject<any, any, any>>(zodDecoder: Z) =>
+  (transform: (z: Z) => (a: any) => A): Codec<A, Z> => ({
+    encode,
+    decode: transform(zodDecoder),
+    zodDecoder,
+  })
+
+const encodeVoid = () => ({
+  type: "Void",
+})
+
+const voidDecoder = z.object({ type: z.literal("Void") })
+
+const Void = buildZodCodec<void>(encodeVoid)(voidDecoder)(
+  (_) => _.transform(() => undefined).parse,
+)
+
+console.log(Void.encode())
+console.log(Void.decode({ type: "Void" }))
+// console.log(Void.decode({ type: "Void2" }))
+const encodeBool = (a: boolean) => ({
+  type: "Bool",
+  value: a,
+})
+
+const boolDecoder = z.object({ type: z.literal("Bool"), value: z.boolean() })
+
+const Bool = buildZodCodec<boolean>(encodeBool)(boolDecoder)(
+  (_) => _.transform((a) => a.value).parse,
+)
+
+console.log(Bool.encode(true))
+console.log(Bool.encode(false))
+console.log(Bool.decode({ type: "Bool", value: true }))
+console.log(Bool.decode({ type: "Bool", value: false }))
+// console.log(Bool.decode({ type: "Bool", value: "true" }))
+
+const encodeString = (a: string) => ({
+  type: "String",
+  value: a,
+})
+
+const stringDecoder = z.object({
+  type: z.literal("String"),
+  value: z.string(),
+})
+
+const String = buildZodCodec<string>(encodeString)(stringDecoder)(
+  (_) => _.transform((a) => a.value).parse,
+)
+
+console.log(String.encode("hello"))
+console.log(String.decode({ type: "String", value: "hello" }))
+// console.log(String.decode({ type: "String", value: 1 }))
+
+const encodeOptional = <A>(a: A | null, resolve?: IdentifierResolver) => ({
+  type: "Optional",
+  value: a === null ? null : String.encode(a, resolve),
+})
+
+console.log(Optional(String).encode("hello"))
+console.log(Optional(String).encode(null))
+console.log(Optional(String).decode({ type: "Optional", value: "hello" }))
+console.log(Optional(String).decode({ type: "Optional", value: null }))
+console.log(Optional(String).decode({ type: "Optional", value: 1 }))
+
+/*
+
+Example syntax:
+
+const encoder =
+  Codec.struct({
+
+  })
+
+
+*/
